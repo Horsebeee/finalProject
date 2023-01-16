b98b277b40e1e3bee9ff08ce64de87267b571d84
Horsebeee <jyjun83@naver.com>
2023-01-16 23:27:49 +0900

    #5 Thymeleaf 의존성 추가 및 설정
    - ThymeleafConfig 파일 추가
       - Thymeleaf3Properties 사용하기 위해서
    - yml 에 spring.thymeleaf3: decoupled-logic: true 추가
    - FinalApplication.java 에 @ConfigurationPropertiesScan 어노테이션 추가 이거 안달아주면 ThymeleafConfig 파일 에러남

--- src/main/java/com/study/app/config/SecurityConfig.java
+++ src/main/java/com/study/app/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig {
                         - URL 접근 권한 설정
                         - 커스텀 로그인 페이지 지원
                         - 인증 후 성공/실패 핸들링
-                        - 사용자 로그인/로그아웃itAll())
+                        - 사용자 로그인 / 로그아웃 itAll())
                 .formLogin()
                 - CSRF 공격으로부터 보호
         */
@@ -21,16 +21,17 @@ public class SecurityConfig {
         return http // 제한 해제하는 부분
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .formLogin()
+                .loginPage("/login.html")
                 .and().build();
 
 
 
         /** formLogin() 이후 사용할 수 있는 메서드
-         http.formLogin()
-         .loginPage("/login.html")       // 커스텀 로그인 페이지 보여줄때
-         .defaultSuccessUrl("/index")    // 로그인 성공 후 이동할 페이지 경로
-         .failureUrl("/login.html?error=true") // 로그인 실패 후 이동할 페이지
-         .usernameParameter("유저네임")      // 아이디 파라미터명 설정
+             http.formLogin()
+             .loginPage("/login.html")       // 커스텀 로그인 페이지 보여줄때
+             .defaultSuccessUrl("/index")    // 로그인 성공 후 이동할 페이지 경로
+             .failureUrl("/login.html?error=true") // 로그인 실패 후 이동할 페이지
+             .usernameParameter("유저네임")      // 아이디 파라미터명 설정
 
          */
     }
