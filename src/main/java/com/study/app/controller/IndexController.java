667f000db6a50e9272353cdbd6b3fccb7f28e154
Horsebeee <jyjun83@naver.com>
2023-01-16 23:38:26 +0900

    #13 sns 서비스 view 구현
    - 로그인페이지 구현 (html,css)
    - 회원가입 페이지 구현 (html,css)
    - 메인페이지 구현 (html,css)
    
    - 컨트롤러를 통해 화면 나오는지 확인했음

--- src/main/java/com/study/app/controller/IndexController.java
+++ src/main/java/com/study/app/controller/IndexController.java
@@ -1,12 +1,23 @@
 package com.study.app.controller;
 
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
     @GetMapping("/")
     public String index() {
-        return "Hello";
+        return "index";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
     }
 }
